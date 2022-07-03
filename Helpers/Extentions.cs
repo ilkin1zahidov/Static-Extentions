b50aa8dd412e2b785585048a10b9787a35d5287c
ilkin1zahidov <ilkiniz@code.edu.az>
2022-07-03 19:51:06 +0400

    Extentions

---
+++ Helpers/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Extentions.Helpers
+{
+   public static class Extentions
+   {
+        public static double Pow(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+              
+        }
+        
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Static_Extentions.Helpers;
+using System;
+
+namespace Static_Extentions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 5;
+            double num2 = 2;
+
+          var sum = Math.Pow(num1, num2);
+            
+
+            Console.WriteLine(sum);
+
+
+            
+        }
+    }
+}
